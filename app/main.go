be5d5adea1abe1e0297088dba080ee20a197d812
Andrey Yaysh <and@yarysh.com>
2021-05-23 23:42:36 +0200

    IBAN Server - done

--- app/main.go
+++ app/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/yarysh/iban-service/app/iban/validator"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	validator.Validate("")
+	var addr string
+	flag.StringVar(&addr, "addr", ":8080", "server addr")
+	flag.Parse()
+
+	server := NewServer()
+	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ibanParam, ok := r.URL.Query()["iban"]
+		if !ok || len(ibanParam) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(`{"error": "iban param required"}`))
+			return
+		}
+		iban := ibanParam[0]
+		iban = strings.ReplaceAll(iban, " ", "")
+		iban = strings.ToUpper(iban)
+		valid := validator.Validate(iban)
+		w.Write([]byte(fmt.Sprintf(`{"iban": "%s", "valid": %t}`, iban, valid)))
+	})
+
+	fmt.Printf("Starting server at %s...\n", addr)
+	err := http.ListenAndServe(addr, server)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- app/server.go
+++ app/server.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	mux *http.ServeMux
+}
+
+func (s *Server) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	s.mux.HandleFunc(pattern, handler)
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Access logs
+	handler := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+			log.Printf("[%s] %s, %s\n", r.Method, r.RemoteAddr, r.URL.Path)
+		})
+	}(s.mux)
+
+	// Panic resolver
+	handler = func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(`{"error": "internal server error"}`))
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}(handler)
+
+	// JSON response
+	handler = func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			next.ServeHTTP(w, r)
+		})
+	}(handler)
+
+	handler.ServeHTTP(w, r)
+}
+
+func NewServer() *Server {
+	return &Server{mux: new(http.ServeMux)}
+}
