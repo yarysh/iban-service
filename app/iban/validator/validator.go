53ff0e2fd047bbad59c85286fa8932b9bb5baefb
Andrey Yaysh <and@yarysh.com>
2021-05-23 22:17:07 +0200

    IBAN Validator - done

--- app/iban/validator/validator.go
+++ app/iban/validator/validator.go
@@ -1,13 +1,43 @@
 package validator
 
-import (
-	"fmt"
-)
+const MOD = 97
 
 //go:generate go run "github.com/yarysh/iban-service/app/iban" gen_validator_rules
 func Validate(iban string) bool {
-	for _, rule := range Rules {
-		fmt.Println(rule)
+	ibanLen := len(iban)
+	if ibanLen < 2 {
+		return false
 	}
-	return false
+
+	country := iban[0:2]
+	rule, ok := Rules[country]
+	if !ok || ibanLen != rule.Size {
+		return false
+	}
+
+	if !rule.Regex.MatchString(iban) {
+		return false
+	}
+
+	total := 0
+	pos, i := 4, 0
+	for ; i < ibanLen; i++ {
+		if pos == ibanLen {
+			pos = 0
+		}
+		char := rune(iban[pos])
+		var n int
+		if '0' <= char && char <= '9' {
+			n = int(char) - '0'
+		} else if 'A' <= char && char <= 'Z' {
+			n = 10 + int(char) - 'A'
+		}
+		if n > 9 {
+			total = (100*total + n) % MOD
+		} else {
+			total = (10*total + n) % MOD
+		}
+		pos++
+	}
+	return total == 1
 }
